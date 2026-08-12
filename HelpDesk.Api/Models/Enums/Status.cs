e2fcbe7581fb7e5552fb3e8a368450b27cfcbed2
Bahaa Mohamed <bahaa-dev@proton.me>
2026-08-12 16:32:02 +0300

    refactor(enum): use JsonStringEnumConverter for Status and VoteValue serialization

--- HelpDesk.Api/Models/Enums/Status.cs
+++ HelpDesk.Api/Models/Enums/Status.cs
@@ -1,5 +1,8 @@
+using System.Text.Json.Serialization;
+
 namespace HelpDesk.Api.Models.Enums;
 
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum Status
 {
     Open,
--- HelpDesk.Api/Models/Enums/VoteValue.cs
+++ HelpDesk.Api/Models/Enums/VoteValue.cs
@@ -1,7 +1,10 @@
+using System.Text.Json.Serialization;
+
 namespace HelpDesk.Api.Models.Enums;
 
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum VoteValue
 {
     Downvote = -1,
-    Upvote = 1,
+    Upvote = 1
 }

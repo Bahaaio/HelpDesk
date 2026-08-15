703600083f845819b01b2192a6e7c504b540ef20
Bahaa Mohamed <bahaa-dev@proton.me>
2026-08-15 14:12:11 +0300

    refactor(dtos): use init instead of set in ticketDto

--- HelpDesk.Api/Dtos/Responses/TicketDto.cs
+++ HelpDesk.Api/Dtos/Responses/TicketDto.cs
@@ -4,15 +4,15 @@ namespace HelpDesk.Api.Dtos.Responses;
 
 public record TicketDto
 {
-    public required int Id { get; set; }
-    public required string Title { get; set; }
-    public required string? Description { get; set; }
-    public required Status Status { get; set; }
-    public required DateTime CreatedAt { get; set; }
-    public required string AuthorUsername { get; set; }
+    public required int Id { get; init; }
+    public required string Title { get; init; }
+    public required string? Description { get; init; }
+    public required Status Status { get; init; }
+    public required DateTime CreatedAt { get; init; }
+    public required string AuthorUsername { get; init; }
 
-    public required List<string> Tags { get; set; }
-    public required List<Guid> Attachments { get; set; }
+    public required List<string> Tags { get; init; }
+    public required List<Guid> Attachments { get; init; }
 
-    public required int VoteScore { get; set; }
+    public required int VoteScore { get; init; }
 }
